3086e053ac2ca0b38f5b2ace9615374b0cfb78fd
Ward Slager <3001621+wjslager@users.noreply.github.com>
2018-10-12 12:07:17 +0200

    Added 1337 matrix

---
+++ public/conductor_rhythm/matrix.js
@@ -0,0 +1,132 @@
+const rainDropSize = 32;
+var rain = [];
+var rainSpeed = 5;
+
+var input = '';
+
+var hackingProgress = 0;
+
+
+// ================ PRELOAD
+function matrixPreload()
+{
+	rainFont = loadFont('assets/matrix code nfi.otf');
+	monoFont = loadFont('../assets/dejavu/ttf/DejaVuSansMono.ttf');
+}
+
+
+// ================ SETUP
+function matrixSetup()
+{
+	// Let it rain
+	for (var i=0; i<50; i++)
+		// rain.push(new RainDrop());
+
+	background(0);
+	// rectMode(CENTER);
+	textAlign(CENTER, CENTER);
+}
+
+
+// ================ DRAW
+function matrixDraw()
+{
+	background(0, 10);
+
+	textFont(rainFont);
+	textSize(rainDropSize * 0.9);
+
+	if ((frameCount % rainSpeed) === 0)
+		for (var i=0; i<rain.length; i++)
+			rain[i].display();
+
+	// Codebox
+	fill(0);
+	stroke(0, 255, 0);
+	rect(windowWidth*.5 - 200,windowHeight*.5 - 50, 400, 100);
+
+
+	// Hacker text
+	fill(0, 255, 0);
+	textFont(monoFont);
+	text(input, windowWidth*.5,windowHeight*.5);
+
+	// Progress bar
+	rect(windowWidth*.5 - 200,windowHeight*.5 - 50, hackingProgress * 4, 10);
+
+	hackingProgress = hackingProgress * 0.99;
+
+	if (hackingProgress > 100 && random(100) > 90)
+		background(random(255));
+}
+
+function RainDrop()
+{
+	this.x = int(random(width / rainDropSize)) * rainDropSize;
+	this.y = int(random(height / rainDropSize)) * rainDropSize;
+	this.char = char(32+random(32));
+	this.age = 0;
+
+	this.display = function()
+	{
+		if (hackingProgress < 100)
+			fill(100, 255, 100);
+		else
+			fill(255);
+
+		// New char on each draw
+		this.char = char(32+random(32));
+		text(this.char, this.x, this.y);
+
+		// Move and keep within window
+		this.y += rainDropSize;
+		if (this.y > height)
+			this.y = 0;
+
+		// Small chance to move
+		if (random(100) > 90 && this.y === 0)
+			this.x = int(random(width / rainDropSize)) * rainDropSize;
+		
+		// Age and kill
+		this.age++;
+		if (this.age > random(100))
+			rain.pop();
+	}
+}
+
+// ================ KEYBOARD
+function keyPressed()
+{
+	// ARTIFICIAL INTELLIGENCE
+	switch(keyCode)
+	{
+		case BACKSPACE:
+			input = input.substring(0, input.length-1);
+			break;
+
+		case RETURN:
+			
+			for (var i=0; i<100; i++)
+				rain.push(new RainDrop());
+
+			hackingProgress += (input.length) * 10;
+
+			input = '';
+
+			break;	
+	}
+}
+
+function keyTyped()
+{
+	// machin lerning
+	rain.push(new RainDrop());
+
+	// Filter out non keyboard characters and constrain length
+	if (keyCode >= 32 && keyCode <= 126 && input.length < 20)
+		input += key.toUpperCase();
+
+	
+}
+
+
--- public/conductor_rhythm/sketch_rhythm.js
+++ public/conductor_rhythm/sketch_rhythm.js
@@ -1,119 +1,31 @@
 var socket;
 
-// Values we're going to send to the server
-var param1 = 0;
-var param2 = 0;
-
-var balls = [];
-var ballSize = 200;
-var ballRad = ballSize / 2;
+// ================ PRELOAD
+function preload()
+{
+	matrixPreload();
+}
 
 
 // ================ SETUP
 function setup()
 {
 	createCanvas(displayWidth, displayHeight);
-	socket = io.connect("http://localhost:3000/conductor_rhythm/");
+	socket = io.connect("http://localhost:3000");
 
-	mouseX = width/2;
-	mouseY = height/2;
-
-	background(0);
-	strokeWeight(10);
-	stroke(255);
-	fill(0, 50);
-
-
-
+	matrixSetup();
 }
-
 
 // ================ DRAW
 function draw()
 {
-	background(0, 50);
-
-	for (var i=0; i<balls.length; i++)
-		balls[i].display();
-
-	// Stuur deze twee parameters door
-	param1 = balls.length;
-	param2 = param2 * 0.99;
+	matrixDraw();
 }
 
 
-// ================ INTERFACE
-function mousePressed()
-{
-	balls.push(new Ball());
-
-	socket.emit('ball', {
-		x: param1,
-		y: param2
-	});
-}
-
+// ================ INTERFACES
 function windowResized()
 {
-	resizeCanvas(windowWidth, windowHeight)
-}
-
-
-// ================ BALL BEHAVIOUR
-function Ball()
-{
-	this.x = mouseX;
-	this.y = mouseY;
-	this.xSpeed = random(-5, 5);
-	this.ySpeed = random(-5, 5);
-	this.strength = 1;
-	this.killCount = 0;
-
-	this.display = function()
-	{
-		stroke(10 * this.strength + 100, 10 * this.strength + 50, 100 + 20 * this.strength);
-		ellipse(this.x, this.y, ballSize, ballSize);
-		this.move();
-		this.collide();
-	};
-
-	this.move = function()
-	{
-		this.x += this.xSpeed;
-		this.y += this.ySpeed;
-
-		// Check for screenborder collision
-		if (this.x > (windowWidth - ballRad) || this.x < ballRad) this.xSpeed *= -1;
-		if (this.y > (windowHeight - ballRad) || this.y < ballRad) this.ySpeed *= -1;
-	}
-
-	this.collide = function()
-	{
-		// Check with every existing ball
-		for (var i=0; i<balls.length; i++)
-		{
-			var ballDist = dist(this.x, this.y, balls[i].x, balls[i].y);
-
-			// Check for collision and if we're not comparing against ourselves
-			if (ballDist > 0 && ballDist < ballSize)
-			{
-				// Absorb values of the ball we ran into
-				this.strength += balls[i].strength;
-				this.xSpeed += balls[i].xSpeed;
-				this.ySpeed += balls[i].ySpeed;
-				this.killCount += balls[i].killCount;
-
-				// Remove the ball we ran into
-				balls.splice(i, 1);
-
-				// Celebrate the victory
-				param2 += 1;
-				this.killCount++;
-
-				// Kill a random ball if a certain ball proves to be a real killer
-				if (this.killCount > 5)
-					balls.pop();
-			}
-		}
-	}
+	resizeCanvas(windowWidth, windowHeight);
+	background(0);
 }
