5483a57e065bc077d18bc5d23600c0273b3b0b2d
BramGiesen <giesenbram@gmail.com>
2018-10-15 17:20:23 +0200

    changed host

--- public/server_settings/setup.js
+++ public/server_settings/setup.js
@@ -1,4 +1,3 @@
-const hostname = "192.168.0.100";    //default: 192.168.0.100 For development change this to localhost
+const hostname = "localhost";    //default: 192.168.0.100 For development change this to localhost
 
 const port = 3000;                          //default: 3000
-
